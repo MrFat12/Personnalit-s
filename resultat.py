74a3bbb70b6ec2041b744fa30db5c27d89acd5c2
Andrea <brechbuhlandrea@gmail.com>
2025-03-12 19:14:22 +0100

    Améliration de la page d'accueil avec ajout texte et bacjground entre autre

---
+++ resultat.py
@@ -0,0 +1,6 @@
+resultats = {
+        "Builder" : "Tu es quelqu'un de créatif et de très original !",
+        "Redstonneur" : "Tu es quelqu'un doté d'une très grande logique et tu adore résoudre les problèmes complexes.",
+        "Mineur" : "Toi, tu peux passer facilement des heures sur un sujet tant que tu n'es pas satifais, tu es extrèmement motivé pour quoi que ce soit.",
+        "Joueur PvP" : "Tu es quelqu'un de très actif et tu aime vivre au jour le jour.",
+    }
